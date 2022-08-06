9c60ee4c7cdfc2e96bfb4703a9619809d0fb77af
Ramiro Ramirez <ramiro@kryha.io>
2022-08-06 09:32:49 +0200

    Update logs

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ import naive_em
 import em
 
 X = np.loadtxt("toy_data.txt")
-# TODO: Your code here
+
+#
 K = 4
 seed = 4
-(mixture, post) = common.init(X=X, K=K, seed=seed)
 
+(mixture, post) = common.init(X=X, K=K, seed=seed)
 (mixture, post, cost) = kmeans.run(X=X, mixture=mixture, post=post)
 
-print("K: ", K)
-print("Seed: ", seed)
-print("Cost: ", cost)
-
 plot_title = f"K={K} Seed={seed} Cost={cost}"
+print(plot_title)
 common.plot(X=X, mixture=mixture, post=post,
             title=plot_title)
