05d165ca81a7c81d692ca091b6385f3af370f638
Ramiro Ramirez <ramiro@kryha.io>
2022-08-06 09:31:18 +0200

    Add Kmeans run to Main

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ import naive_em
 import em
 
 X = np.loadtxt("toy_data.txt")
-
 # TODO: Your code here
+K = 4
+seed = 4
+(mixture, post) = common.init(X=X, K=K, seed=seed)
+
+(mixture, post, cost) = kmeans.run(X=X, mixture=mixture, post=post)
+
+print("K: ", K)
+print("Seed: ", seed)
+print("Cost: ", cost)
+
+plot_title = f"K={K} Seed={seed} Cost={cost}"
+common.plot(X=X, mixture=mixture, post=post,
+            title=plot_title)
